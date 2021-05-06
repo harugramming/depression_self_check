c63a2dd3f89fef52f9ecb680dc356258cf60ba87
春内悠甫 <>
2021-05-06 19:54:00 +0900

    サイドバーを実装しました。

--- work/public/index.php
+++ work/public/index.php
@@ -37,6 +37,22 @@ session_start();
     </header>
     <!-- Header end -->
     
+    <!-- < side bar start> -->
+    <div id="side_menu">
+      <div class="menu_option">
+        <i class="fas fa-clipboard-list"></i>
+        <p>チェック</p>
+      </div>
+      <div class="menu_option">
+        <i class="fas fa-edit"></i>
+        <p>編集</p>
+      </div>
+      <div class="menu_option">
+        <i class="far fa-chart-bar"></i>
+        <p>グラフ</p>
+      </div>
+    </div>
+    <!-- < side bar end> -->
     <!-- Check sheet start -->
     <div class="check_sheet_container">
       <table class="check_sheet_header">
