198cba2e69ab0a1d66982505d882f0b099c4a21a
春内悠甫 <>
2021-05-11 13:15:55 +0900

    編集画面において、DBの内容を反映

--- work/app/php/_checksheet.php
+++ work/app/php/_checksheet.php
@@ -14,7 +14,6 @@
 
     
     <?php  
-
     
     require_once('config.php');
 
--- work/public/edit.php
+++ work/public/edit.php
@@ -18,6 +18,27 @@
 
   ?>
 
+<?php  
+    
+    require_once('config.php');
+
+    //チェックシートの項目を格納する配列
+    $contents = array();
+
+    //　接続
+    $pdo = new PDO(DSN, DB_USER, DB_PASS);
+    $sql = "SELECT * FROM checksheet WHERE id = :id ORDER BY number";
+    $stmt = $pdo->prepare($sql);
+    $params = array(':id' => $_SESSION['ID']);
+    $stmt->execute($params);
+
+    // foreach文で配列の中身を一行ずつ代入
+    foreach ($stmt as $row) {
+      $contents[] = $row['contents'];
+    };
+
+    ?>
+
    <!-- Check sheet start -->
    <div class="check_sheet_container">
       <table class="check_sheet_header">
@@ -31,46 +52,22 @@
           </tr>
         </tbody>
       </table>
-        <table class="check_table">
-          <tbody>
-            <tr id="question1">
-              <th class="check_table_number">1</th>
-              <td class="check_table_text">
-                <input type="text" id="check_content1" value="ああああ">
-              </td>
-              <td><i class="fas fa-times check_delete"></i></td>
-            </tr>
-          </tbody>
-        </table>
-        <table class="check_table">
-          <tbody>
-            <tr id="question2">
-              <th class="check_table_number">2</th>
-              <td class="check_table_text">
-                <input type="text" id="check_content2" value="いいいい">
-              </td>
-              <td><i class="fas fa-times check_delete"></i></td>
-            </tr>
-          </tbody>
-        </table>
-        <table class="check_table">
-          <tbody>
-            <tr id="question3">
-              <th class="check_table_number">3</th>
-              <td class="check_table_text"><input type="text" id="check_content3" value="うううう"></td>
-              <td><i class="fas fa-times check_delete"></i></td>
-            </tr>
-          </tbody>
-        </table>
-        <table class="check_table">
-          <tbody>
-            <tr id="question4">
-              <th class="check_table_number">4</th>
-              <td class="check_table_text"><input type="text" id="check_content4" value="ええええ"></td>
-              <td><i class="fas fa-times check_delete"></i></td>
-            </tr>
-          </tbody>
-        </table>
+<?php
+  foreach($contents as $index => $content){
+    echo "<table class='check_table'>
+            <tbody>
+              <tr id='question" . ($index + 1) . "'>
+                <th class='check_table_number'>" . ($index + 1) . "</th>
+                <td class='check_table_text'>
+                  <input type='text' id='check_content" . ($index + 1) . "' value='" . $content . "'>
+                </td>
+                <td><i class='fas fa-times check_delete'></i></td>
+              </tr>
+             </tbody>
+          </table>";
+  };
+?>
+ 
     </div>
     <div id="button_container">
       <a id="check_add" class="btn-square-shadow">追加</a>
